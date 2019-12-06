758ee5ab19d1c13c4c7216ece9032d8b61550304
jnefta91 <e.josem91@gmail.com>
2019-12-06 10:51:29 -0600

    add play pause Reproduction

--- app/src/main/java/com/example/joseramirez/applaudoplayer/MainActivity.kt
+++ app/src/main/java/com/example/joseramirez/applaudoplayer/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.joseramirez.applaudoplayer
 
-import android.support.v7.app.AppCompatActivity
+import android.content.res.Configuration
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    lateinit var btnPlay    : Button
+    lateinit var btnPause   : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnPlay = findViewById(R.id.btnPlay)
+        btnPause = findViewById(R.id.btnPause)
+
+        mediaPlayer = MediaPlayer.create(this ,R.raw.eminem)
+
+        btnPlay.setOnClickListener {
+            mediaPlayer.start()
+        }
+
+        btnPause.setOnClickListener {
+            mediaPlayer.pause()
+        }
     }
+    
+
+
+
+
 }
