7723ad821c3468aa906e061f05239099f42edde8
jnefta91 <e.josem91@gmail.com>
2019-12-09 11:49:56 -0600

    add Activity for details song

---
+++ app/src/main/java/com/example/joseramirez/applaudoplayer/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.joseramirez.applaudoplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+    }
+}
