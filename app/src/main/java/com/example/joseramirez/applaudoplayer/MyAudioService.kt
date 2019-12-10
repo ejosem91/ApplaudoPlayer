20f66981d47525d5dc4c81292299642867ed589a
jnefta91 <e.josem91@gmail.com>
2019-12-09 18:33:53 -0600

    add pause and play from notification bar

--- app/src/main/java/com/example/joseramirez/applaudoplayer/MyAudioService.kt
+++ app/src/main/java/com/example/joseramirez/applaudoplayer/MyAudioService.kt
@@ -2,9 +2,13 @@
 
 package com.example.joseramirez.applaudoplayer
 
-import android.app.*
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.media.MediaPlayer
 import android.os.Binder
 import android.os.Build
@@ -12,16 +16,13 @@ import android.os.IBinder
 import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 
-
 class MyAudioService : Service() {
 
-    private val packageNameProject = "com.example.joseramirez.applaudoplayer"
+    private lateinit var notificationManager: NotificationManager
     private val binder = LocalBinder()
     private var mediaPlayer: MediaPlayer? = null
 
     //notifications declarations
-    private var notificationManager : NotificationManager? = null
-
     inner class LocalBinder : Binder() {
         // Return this instance of LocalService so clients can call public methods
         fun getService(): MyAudioService = this@MyAudioService
@@ -31,8 +32,16 @@ class MyAudioService : Service() {
         return binder
     }
 
-
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val flagIntent = intent?.action
+
+        if (flagIntent.equals(PAUSE)) {
+            pausePlayer()
+        } else if (flagIntent.equals(PLAY)) {
+            createPlayer()
+        }
+
         return START_NOT_STICKY
     }
 
@@ -41,8 +50,13 @@ class MyAudioService : Service() {
     fun createPlayer() {
         if (mediaPlayer == null) {
             mediaPlayer = MediaPlayer.create(this, R.raw.eminem)
-            notificationManager= getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            createNotificationChannel(packageNameProject, "Listen Music", "Playing Song")
+            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            setConfigsNotify()
+            createNotificationChannel(
+                CHANNEL_ID,
+                ACTION,
+                DESCRIPTION
+            )
         }
         mediaPlayer?.start()
     }
@@ -51,17 +65,60 @@ class MyAudioService : Service() {
         mediaPlayer?.pause()
     }
 
+    private fun setConfigsNotify() {
+
+        val playIntent = Intent(this, this::class.java).apply {
+            action = PLAY
+        }
+
+        val playPendingIntent: PendingIntent =
+            PendingIntent.getService(this, 0, playIntent, 0)
+
+        val pauseIntent = Intent(this, this::class.java).apply {
+            action = PAUSE
+        }
+
+        val pausePendingIntent: PendingIntent =
+            PendingIntent.getService(this, 0, pauseIntent, 0)
+
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_info)
+            .setContentTitle(NAME)
+            .setContentText(SONG)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(playPendingIntent)
+            .addAction(R.drawable.ic_play, getString(R.string.play), playPendingIntent)
+            .addAction(R.drawable.ic_pause, getString(R.string.pause), pausePendingIntent)
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+
+    }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun createNotificationChannel(id : String , name : String , description: String) {
-        val notification = NotificationCompat.Builder(this)
-            .setContentTitle(name)
-            .setContentText(description)
-            .setSmallIcon(android.R.drawable.ic_dialog_info)
-            .setChannelId(id)
-            .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
-            .build()
-        notificationManager?.notify(101, notification)
+    private fun createNotificationChannel(
+        id: String?,
+        name: String?,
+        description: String?
+    ) {
+        val importance = NotificationManager.IMPORTANCE_LOW
+        val channel = NotificationChannel(id, name, importance)
+        channel.description = description
+        channel.enableLights(true)
+        channel.lightColor = Color.RED
+        channel.enableVibration(true)
+        channel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        const val NOTIFICATION_ID = 20
+        const val DESCRIPTION = "Play"
+        const val ACTION = "Notify"
+        const val SONG = "Lose your self"
+        const val NAME = "APPLAUDOFY"
+        const val CHANNEL_ID = "channelPlayer"
+        const val PAUSE = "PAUSE"
+        const val PLAY = "PLAY"
     }
 
 }
